738eaa27bbff718aa692be773e0fb88c414fb413
meanseo <chlalstjok97@gmail.com>
2022-04-04 18:14:20 +0900

    first commit

---
+++ app/routes/users.routes.js
@@ -0,0 +1,19 @@
+/** 
+var express = require('express');
+var router = express.Router();
+GET users listing. 
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+*/
+module.exports = function(app){
+  app.use((req, res, next) => {
+    res.header(
+
+    )
+  });
+  // next();
+  app.get("", () => {})
+  app.update("", () => {})
+  app.delete("", () => {})
+};
