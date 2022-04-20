d86f6747e9af53c38f24aca82b5ffea4291f4669
meanseo <chlalstjok97@gmail.com>
2022-04-20 11:49:10 +0900

    new branch

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 const tokenRouter = require('./app/routes/token');
 app.use('/token', tokenRouter);
-// const { verifyToken } = require('./middlewares');
+
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
